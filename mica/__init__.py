34e25b31257731b28d77e2333b35a7f8bfb33d37
kenxie7 <kk.xie419@gmail.com>
2025-12-22 09:57:57 +0200

    updated repo

--- mica/__init__.py
+++ mica/__init__.py
@@ -19,7 +19,7 @@ Usage
 """
 
 __version__ = '0.1.0'
-__author__ = 'MiCA Team'
+__author__ = 'Ken Xie'
 
 from .projector import EdgeProjector, ProjectionResult, load_data
 from .plotting import ProjectionPlotter
